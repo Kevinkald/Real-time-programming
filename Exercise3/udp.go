0e81628daed025d46016cfa67ff8fd88f5a3b59a
Adrian Ghansah <adrianghnsh@gmail.com>
2019-02-01 16:24:58 +0100

    Exercise4

--- Exercise3/udp.go
+++ Exercise3/udp.go
@@ -1,49 +1,45 @@
 package main
 
 import(
-	. "fmt"
+	"fmt"
 	"runtime"
 	"net"
 	"log"
 	"time"
 )
 
-func receive(portnumber string) {
-	pc, err := net.ListenPacket("udp",portnumber)
+func receive(portnumber int) {
+	addr := net.UDPAddr{Port: portnumber}
+	pc, err := net.ListenUDP("udp", &addr)
+	if err != nil{
+		fmt.Println(err)
+	}
+	defer pc.Close()
+	buffer := make([]byte, 1024)
 	for {
-		
-		if err != nil{
-			log.Fatal(err)
+		n, err2 := pc.Read(buffer)
+		if err2!= nil{
+			fmt.Println(err)
 		}
-		defer pc.Close()
-	
-		buffer := make([]byte, 1024)
-		Print(pc.ReadFrom(buffer))
-
-		time.Sleep(500* time.Millisecond)
+		fmt.Println(string(buffer[:n]))
 	}
 }
 
 
 func sender(finished chan<- bool){
-	// broadcastIP = #.#.#.255. First three bytes are from the local IP, or just use 255.255.255.255
-	//addr := UDPAddr{IP: 10.100.23.242, Port: 20012, Zone: ""}
-	//addr = new InternetAddress(broadcastIP, port)
-	Conn, err := net.Dial("udp", "10.100.23.242:20012")
-	for {
-		if err != nil {
+	//Conn, err := net.Dial("udp", "10.100.23.242:20012")
+	Conn, err := net.Dial("udp","255.255.255.255:20012")
+	if err != nil {
 			log.Fatal(err)
 		}
-		defer Conn.Close()
-		Conn.Write([]byte("Hello"))
+	defer Conn.Close()
+	for {
+		Conn.Write([]byte("Hello!!!!!!!!!!"))
 		time.Sleep(500 * time.Millisecond)
 
 	}
 
 	finished <- true
-	//sendSock = new Socket(udp) // UDP, aka SOCK_DGRAM
-	//sendSock.setOption(broadcast, true)
-	//sendSock.sendTo(message, 10.100.23.242:58938)
 }
 
 
@@ -55,17 +51,11 @@ func main(){
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	
 	finished := make(chan bool)
-	//1
-	//receive(":30000")
 
-	//2
 	go sender(finished)
-	go receive(":20012")
+	go receive(20012)
 
 	<- finished
-	//go receive()
-
-	//receive()
 	
 }
 
